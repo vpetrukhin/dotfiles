89c8f4dc776faa1086dfff92c515450f31a794a7
Петрухин Василий <vpetrukhin@samokat.ru>
2024-11-27 17:41:56 +0300

    add intalling ripgrep

--- install/install-brew-package.sh
+++ install/install-brew-package.sh
@@ -2,6 +2,7 @@ brew install --cask alacritty
 brew install font-meslo-lg-nerd-font
 brew install jesseduffield/lazygit/lazygit
 brew install tmux
+brew install ripgrep
 brew install neovim
 brew install nvm 
 brew install node
