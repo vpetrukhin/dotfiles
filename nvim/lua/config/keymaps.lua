fb9026aa0a835c1a1e0c579bd34242b9cb781e21
Петрухин Василий <vpetrukhin@samokat.ru>
2025-11-07 13:54:31 +0300

    add yank filepath keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<leader>fy", function()
+  local path = vim.fn.expand("%:.")
+  local cmd = "echo '" .. path .. "' | pbcopy"
+  vim.fn.system(cmd)
+  vim.notify("Путь скопирован: " .. path, vim.log.levels.INFO)
+end, { desc = "Копировать путь к файлу в буфер" })
